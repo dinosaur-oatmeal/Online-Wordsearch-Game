b639aad65a5901aa6ebdfb4e2b9778d297938d8d
GraceDaily <123136176+GraceDaily@users.noreply.github.com>
2024-03-16 22:20:54 -0500

    Add files via upload

---
+++ GameSession.java
@@ -0,0 +1,36 @@
+package uta.cse3310;
+import WordBank;
+pubic class GameSession
+{
+	public void startGame()
+	{
+		//will start the game
+	}
+	public void charSelected(int firstLetter, int lastLetter)
+	{
+		//method of selecting characters
+	}
+	
+	public int wordFound(int firstLetter, int lastLetter)
+	{
+		//method to return when a word is found
+		return 0;
+	}	
+	public void wordColor()
+	{
+		//will be how a color is picked
+	}
+	public void highlightWord()
+	{
+		//will highlight a word the players color
+	}
+	public void gameOver(int numberOfWords)
+	{
+		//method to determine if the game is over
+	}
+	public void update(UserEvent U)
+	{
+		//will update the game when a word is won by a player
+	}
+	
+}
