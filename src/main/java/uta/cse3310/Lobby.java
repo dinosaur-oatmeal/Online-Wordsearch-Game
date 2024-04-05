32cb7c9b0d6ec4df2bdadafd35bb6ee79511cb87
Subodh577 <142836702+Subodh577@users.noreply.github.com>
2024-04-05 16:24:04 -0500

    Update Lobby.java

--- src/main/java/uta/cse3310/Lobby.java
+++ src/main/java/uta/cse3310/Lobby.java
@@ -1,20 +1,50 @@
 package uta.cse3310;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lobby {
-    public void waitingPlayers() {
-        // Implementation goes here
+    private Map<String, String> players; // Nickname, Player
+
+    public Lobby() {
+        players = new HashMap<>();
     }
 
+    public void waitingPlayers() {
+        // Return the list of players waiting in the lobby
+        for (String nickname : players.keySet()) {
+            System.out.println(nickname);
+        }
+    }
+
+
     public void addPlayer(String nickname) {
-        // Implementation goes here
+        // Add a new player to the lobby
+        if (!players.containsKey(nickname)) {
+            players.put(nickname, nickname);
+            System.out.println("Player " + nickname + " added to the lobby.");
+        } else {
+            System.out.println("Player " + nickname + " already exists in the lobby.");
+        }
     }
 
     public String messagePlayer(String nickname, String message) {
-        // Implementation goes here
-        return null;
+        // Send a message to a specific player
+        if (players.containsKey(nickname)) {
+            System.out.println("Sending message to " + nickname + ": " + message);
+            return "Message sent to " + nickname;
+        } else {
+            System.out.println("Player " + nickname + " not found in the lobby.");
+            return "Player not found";
+        }
     }
 
     public void joinMatch(String nickname) {
-        // Implementation goes here
+        if (players.containsKey(nickname)) {
+            players.remove(nickname);
+            System.out.println("Player " + nickname + " has joined a match.");
+        } else {
+            System.out.println("Player " + nickname + " not found in the lobby.");
+        }
     }
 }
