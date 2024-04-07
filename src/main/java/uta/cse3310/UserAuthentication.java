e3891ae72e08c3f77aa1d40e6d780016838a2d1b
ammarrafiqq <143457398+ammarrafiqq@users.noreply.github.com>
2024-04-07 22:38:05 +0000

    changes to userauthentication

--- src/main/java/uta/cse3310/UserAuthentication.java
+++ src/main/java/uta/cse3310/UserAuthentication.java
@@ -1,16 +1,31 @@
 package uta.cse3310;
 
+import java.util.ArrayList;
+
 public class UserAuthentication {
+    ArrayList<String> users = new ArrayList<String>();
     public void login(String nickname) {
         // Implementation goes here
+        if (checkName(nickname)) {
+            System.out.println("Welcome " + nickname);
+        } else {
+            System.out.println("Invalid nickname");
+        }
+
     }
 
     public void newUser(String nickname) {
         // Implementation goes here
+        if (checkName(nickname)) {
+            System.out.println("User already exists");
+        } else {
+            users.add(nickname);
+            System.out.println("Welcome " + nickname);
+        }
     }
 
     public Boolean checkName(String nickname) {
         // Implementation goes here
-        return true;
+        return users.contains(nickname);
     }
 }
