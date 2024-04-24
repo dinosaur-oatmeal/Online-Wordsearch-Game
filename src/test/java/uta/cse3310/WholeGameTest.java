c061ce79feda79e52cacb32eef7bd334d64b4f83
ammarrafiqq <143457398+ammarrafiqq@users.noreply.github.com>
2024-04-24 21:44:41 +0000

    working on the test

--- src/test/java/uta/cse3310/WholeGameTest.java
+++ src/test/java/uta/cse3310/WholeGameTest.java
@@ -5,3 +5,21 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+public class WholeGameTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public WholeGameTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(WholeGameTest.class);
+    }
+}
