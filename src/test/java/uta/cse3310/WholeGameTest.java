5007699866146687b6ed725c9f6c3e8f9822a4c3
ammarrafiqq <143457398+ammarrafiqq@users.noreply.github.com>
2024-04-24 21:45:48 +0000

    adding string update

--- src/test/java/uta/cse3310/WholeGameTest.java
+++ src/test/java/uta/cse3310/WholeGameTest.java
@@ -6,20 +6,33 @@ import junit.framework.TestSuite;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class WholeGameTest extends TestCase {
+public class WholeGameTest extends TestCase 
+{
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public WholeGameTest(String testName) {
+    public WholeGameTest(String testName) 
+    {
         super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite() {
+    public static Test suite() 
+    {
         return new TestSuite(WholeGameTest.class);
+    }
+
+    private String update(GameSession G, String msg) 
+    {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        UserEvent U = gson.fromJson(msg, UserEvent.class);
+        G.update(U);
+        String jsonString = gson.toJson(G);
+        return jsonString;
     }
 }
