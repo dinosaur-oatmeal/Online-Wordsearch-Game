ff9eaf38fa0aa6c8755d5d19437ba1e69dcc4771
ammarrafiqq <143457398+ammarrafiqq@users.noreply.github.com>
2024-04-25 05:55:40 +0000

    adding message.java

---
+++ src/main/java/uta/cse3310/Message.java
@@ -0,0 +1,11 @@
+package uta.cse3310;
+
+public class Message {
+    String sender;
+    String message;
+    
+    public Message (String sender, String message) {
+        this.sender = sender;
+        this.message = message;
+    }
+}
