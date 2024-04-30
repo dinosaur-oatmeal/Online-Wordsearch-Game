1bbe938f64f686fcfee188abcfee68a4031f23af
Subodh577 <142836702+Subodh577@users.noreply.github.com>
2024-04-30 15:16:24 -0500

    Update LeaderboardTest.java

--- src/test/java/uta/cse3310/LeaderboardTest.java
+++ src/test/java/uta/cse3310/LeaderboardTest.java
@@ -1,68 +1,74 @@
 package uta.cse3310;
 
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import java.util.*;
 
 public class LeaderboardTest extends TestCase {
-  private Leaderboard leaderboard;
+    private Leaderboard leaderboard;
 
-  public LeaderboardTest(String testName) {
-    super(testName);
-  }
-  public static Test suite() {
-    return new TestSuite(LeaderboardTest.class);
-  }
+    public LeaderboardTest(String testName) {
+        super(testName);
+    }
 
-  public void setUp() {
-    leaderboard = new Leaderboard();
-  }
+    public static Test suite() {
+        return new TestSuite(LeaderboardTest.class);
+    }
 
-  public void testUpdateLeaderboard() {
-    leaderboard.updateLeaderboard("Player1");
-    leaderboard.updateLeaderboard("Player2");
-    leaderboard.updateLeaderboard("Player1");
+    public void setUp() {
+        leaderboard = new Leaderboard();
+    }
 
-    assertTrue(leaderboard.scores.get("Player1") == 2);
-    assertTrue(leaderboard.scores.get("Player2") == 1);
-    System.out.println("testUpdateLeaderboard completed");
-  }
+    public void testUpdateLeaderboard() {
+        leaderboard.updateLeaderboard("Player1", 1);
+        leaderboard.updateLeaderboard("Player2", 1);
+        leaderboard.updateLeaderboard("Player1", 2);
 
-  public void testDisplayLeaderboard() {
-        leaderboard.updateLeaderboard("Player1");
-        leaderboard.updateLeaderboard("Player2");
-        leaderboard.updateLeaderboard("Player3");
-        leaderboard.updateLeaderboard("Player1");
-        
+        assertTrue(leaderboard.getScores().get("Player1") == 2);
+        assertTrue(leaderboard.getScores().get("Player2") == 1);
+
+        System.out.println("testUpdateLeaderboard completed");
+    }
+
+    public void testDisplayLeaderboard() {
+        leaderboard.updateLeaderboard("Player1", 1);
+        leaderboard.updateLeaderboard("Player2", 2);
+        leaderboard.updateLeaderboard("Player3", 3);
+        leaderboard.updateLeaderboard("Player1", 4);
+
         leaderboard.displayLeaderboard();
-    
-        assertTrue(leaderboard.scores.size() == 3);
+
+        assertTrue(leaderboard.getScores().size() == 3);
+
         System.out.println("testDisplayLeaderboard completed");
     }
 
-  public void testSortedLeaderboard() {
-        leaderboard.updateLeaderboard("Player1");
-        leaderboard.updateLeaderboard("Player2");
-        leaderboard.updateLeaderboard("Player3");
-        leaderboard.updateLeaderboard("Player1");
-        leaderboard.updateLeaderboard("Player2");
+    public void testSortedLeaderboard() {
+        leaderboard.updateLeaderboard("Player1", 1);
+        leaderboard.updateLeaderboard("Player2", 2);
+        leaderboard.updateLeaderboard("Player3", 3);
+        leaderboard.updateLeaderboard("Player1", 4);
+        leaderboard.updateLeaderboard("Player2", 5);
 
         leaderboard.displayLeaderboard();
 
-       Iterator<Map.Entry<String, Integer>> it = leaderboard.scores.entrySet().iterator();
-  Integer prevScore = null;
-  while (it.hasNext()) {
-    Map.Entry<String, Integer> entry = it.next();
-    Integer currentScore = entry.getValue();
-    if (prevScore != null) {
-      assertTrue(prevScore >= currentScore); // Check if previous score is greater than or equal to current
+        Iterator<Map.Entry<String, Integer>> it = leaderboard.getScores().entrySet().iterator();
+        Integer prevScore = null;
+
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> entry = it.next();
+            Integer currentScore = entry.getValue();
+
+            if (prevScore != null) {
+                assertTrue(prevScore >= currentScore); // Check if previous score is greater than or equal to current
+            }
+
+            prevScore = currentScore;
+        }
+
+        System.out.println("testSortedLeaderboard completed");
     }
-    prevScore = currentScore;
-  }
-  System.out.println("testSortedLeaderboard completed");
-}
 }
   
     
