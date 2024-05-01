05d86d9b68d492d5546a6a6e7a07ba5517fdbdea
ammarrafiqq <143457398+ammarrafiqq@users.noreply.github.com>
2024-05-01 08:00:49 +0000

    fixing minor issues

--- src/test/java/uta/cse3310/WholeGameTest.java
+++ src/test/java/uta/cse3310/WholeGameTest.java
@@ -95,12 +95,5 @@ public class WholeGameTest extends TestCase
         assertTrue(result.contains("\"Player1\"")); // check for player1
         assertTrue(result.contains("\"gameId\":0"));
         assertTrue(result.contains("\"Player1Score\":0"));
-
-// Testing player joining
-//        msg = "{\"GameId\":0,\"PlayerIdx\":\"Player1\",\"action\":\"joinMatch\"}";
-//        result = update(game, msg);
-
-//        assertTrue(result.contains("\"Player1\":true"));
-
     }
 }
